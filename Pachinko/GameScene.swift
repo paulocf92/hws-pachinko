b0776579bc5a79a2e057852bf670b5610f05d166
Paulo Filho <pcf.feat@gmail.com>
2022-09-22 14:47:27 -0400

    mini-challenge: center ball deployment vertically

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -82,7 +82,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
                 ball.physicsBody?.restitution = 0.4
                 ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
-                ball.position = location
+                ball.position = CGPoint(x: location.x, y: 768)
                 ball.name = "ball"
                 addChild(ball)
             }
