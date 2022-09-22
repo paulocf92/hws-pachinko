52e9c628b84d6ec74cb57e6ccc9b1a208b43d9b1
Paulo Filho <pcf.feat@gmail.com>
2022-09-22 16:49:35 -0400

    challenge done

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -9,10 +9,31 @@ import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    let ballAssets: [String] = ["ballRed", "ballBlue", "ballCyan", "ballCyan", "ballGreen", "ballGrey", "ballPurple", "ballYellow"]
+    
     var scoreLabel: SKLabelNode!
     var score = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var ballsLeftLabel: SKLabelNode!
+    var ballsLeft = 5 {
+        didSet {
+            ballsLeftLabel.text = "Balls Left: \(ballsLeft)"
+        }
+    }
+    
+    var obstaclesPlaced: Bool = false
+    var obstaclesLabel: SKLabelNode!
+    var obstacles = 0 {
+        didSet {
+            obstaclesLabel.text = "Obstacles: \(obstacles)"
+            
+            if obstacles == 5 {
+                obstaclesPlaced = true
+            }
         }
     }
     
@@ -39,6 +60,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel.horizontalAlignmentMode = .right
         scoreLabel.position = CGPoint(x: 980, y: 700)
         addChild(scoreLabel)
+        
+        ballsLeftLabel = SKLabelNode(fontNamed: "Chalkduster")
+        ballsLeftLabel.text = "Balls Left: 5"
+        ballsLeftLabel.horizontalAlignmentMode = .right
+        ballsLeftLabel.position = CGPoint(x: 980, y: 650)
+        addChild(ballsLeftLabel)
+        
+        obstaclesLabel = SKLabelNode(fontNamed: "Chalkduster")
+        obstaclesLabel.text = "Obstacles: 0"
+        obstaclesLabel.horizontalAlignmentMode = .right
+        obstaclesLabel.position = CGPoint(x: 980, y: 600)
+        addChild(obstaclesLabel)
         
         editLabel = SKLabelNode(fontNamed: "Chalkduster")
         editLabel.text = "Edit"
@@ -72,19 +105,31 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 // Create a box
                 let size = CGSize(width: Int.random(in: 16...128), height: 16)
                 let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
+                
                 box.zRotation = CGFloat.random(in: 0...3)
                 box.position = location
                 box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
                 box.physicsBody?.isDynamic = false
+                box.name = "obstacle"
+                
+                obstacles += 1
+                
                 addChild(box)
             } else {
-                let ball = SKSpriteNode(imageNamed: "ballRed")
-                ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-                ball.physicsBody?.restitution = 0.4
-                ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
-                ball.position = CGPoint(x: location.x, y: 768)
-                ball.name = "ball"
-                addChild(ball)
+                if obstaclesPlaced {
+                    let ballImage = ballAssets[Int.random(in: 0...ballAssets.count-1)]
+                    let ball = SKSpriteNode(imageNamed: ballImage)
+                    
+                    ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+                    ball.physicsBody?.restitution = 0.4
+                    ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
+                    ball.position = CGPoint(x: location.x, y: 768)
+                    ball.name = "ball"
+                    
+                    addChild(ball)
+                } else {
+                    showAlert(message: "Place at least 5 obstacles first!", buttonTitle: "OK")
+                }
             }
         }
     }
@@ -128,10 +173,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func collision(between ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball)
-            score += 1
+            ballsLeft += 1
+            checkResult()
         } else if object.name == "bad" {
             destroy(ball: ball)
-            score -= 1
+            ballsLeft -= 1
+            checkResult()
+        } else if object.name == "obstacle" {
+            destroy(ball: ball)
+            destroy(ball: object)
+            ballsLeft -= 1
+            obstacles -= 1
+            checkResult()
         }
     }
     
@@ -142,6 +195,33 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         ball.removeFromParent()
+    }
+    
+    func checkResult() {
+        let resetGameAction: (UIAlertAction) -> Void = { [weak self] _ in
+            self?.obstacles = 0
+            self?.obstaclesPlaced = false
+            self?.ballsLeft = 5
+            self?.enumerateChildNodes(withName: "obstacle", using: { node, _ in
+                node.removeFromParent()
+            })
+        }
+        
+        if obstacles > 0 && ballsLeft == 0 {
+            showAlert(message: "Aww... you've lost.", buttonTitle: "Retry", action: resetGameAction)
+        }
+        
+        if obstacles == 0 {
+            score += 1
+            showAlert(message: "Yay! Congratulations, you've won!", buttonTitle: "New game", action: resetGameAction)
+        }
+    }
+    
+    func showAlert(message: String, buttonTitle: String, action: ((UIAlertAction) -> Void)? = nil) {
+        let ac = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: buttonTitle, style: .default, handler: action))
+        
+        view?.window?.rootViewController?.present(ac, animated: true)
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
